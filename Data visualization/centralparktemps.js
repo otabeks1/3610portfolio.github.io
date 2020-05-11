8b41d7f283744ad0c0b4bb011d9331d549c5ec9a
Otabek Shavkatov <61434688+otabeks1@users.noreply.github.com>
2020-05-11 14:25:00 -0400

    Update centralparktemps.js

--- Data visualization/centralparktemps.js
+++ Data visualization/centralparktemps.js
@@ -1,1 +1,7 @@
 
+//Average Monthly Temperatures at Central Park for 2019 - https://www.weather.gov/media/okx/Climate/CentralPark/monthlyannualtemp.pdf
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var temps = [32.5, 36.2, 41.7, 55.5, 62.2, 71.7, 79.6, 75.5, 70.4, 59.9, 43.9, 38.3];
+
