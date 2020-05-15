dd16ec408361105355fdbf78a3f05c348997fcc8
Otabek Shavkatov <61434688+otabeks1@users.noreply.github.com>
2020-05-15 11:23:06 -0400

    Update donutflavors.js

--- Data visualization/donutflavors.js
+++ Data visualization/donutflavors.js
@@ -1,7 +1,7 @@
 
 //Favorite Donut Flavors (2018 Survey) - https://www.tellwut.com/surveys/lifestyle/food-drink/392-donut-shops.html
 
-var flavors = ["Chocolate Dip", "Maple Dip", "Honey Glaze", "Donuts with Sprinkles", "Boston Cream", "Other Cream Filled Donuts", "Jelly", "Creullers", "Other"];
+var flavors = ["Chocolate ", "Strawberry", "Vanilla", "Honey", "Cream", "Dark Chocolate", "Jelly", "Apple", "Other"];
 
 var percentages = [20, 8, 14, 7, 21, 8, 9, 7, 7];
 
